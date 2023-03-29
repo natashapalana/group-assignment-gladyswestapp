36da75f9e7611944b15c795fd2c680d0b9e5ffeb
Angelline Gomwe <121594000+angch21@users.noreply.github.com>
2023-03-28 21:22:06 -0700

    Angelline added code for additional functionality

--- natashaandangelUserInterface.py
+++ natashaandangelUserInterface.py
@@ -121,7 +121,87 @@ def runApp(userName, satData):
 # At the end, user can put their inputs and it will generate well by Natasha Palana
 
 #Angelline's code here below:
+userQuit = False
 
+while (not userQuit):
+
+    #menu
+
+    print("\n")
+    print("-------------------------------")
+
+    print("Welcome to Gladys West Map App")
+
+    print("-------------------------------")
+
+    print("Current position     : x = 56 , y = 13")
+
+    print("Destination position : x = 98 , y = 2")
+
+    print("Distance             : 607.87")
+    print("\n")
+
+    print("[c] to set current position")
+
+    print("[d] to set destination position")
+
+    print("[m] to map - which tells the distance")
+
+    print("[t] to run module tests")
+
+    print("[q] to quit")
+
+    print()
+
+        # get first character of input
+
+    userInput = input("Enter a command:")
+
+    lowerInput = userInput.lower()
+
+    firstChar = lowerInput[0:1]
+
+        # menu choices, use a switch-like if-elif control structure
+
+    if firstChar == 'c' or firstChar == 'd':
+
+        xCurrent = int(input("Enter a x position: "))
+
+        yCurrent = int(input("Enter a y position: "))
+
+        xDestination = int(input("Enter a x position: "))
+
+        yDestination = int(input("Enter a y position: "))
+        distance=((((xDestination - xCurrent )**2)+((yDestination-yCurrent)**2))**0.5)
+        print(distance)
+        break
+
+    elif firstChar == 'm':
+
+        print(distance)
+        break
+
+        # quit
+
+    if firstChar == 'q':
+
+        userQuit = True
+
+        # run some tests (this is part 1 of 2)
+
+    elif firstChar == 't':
+
+        runTests()
+
+    else:
+
+        print("ERROR: " + firstChar + " is not a valid command")
+
+print("\n")
+
+print("Thank you for using the Gladys West Map App!")
+
+print("\n")
 
 def compute_distance(current_position, destination_position):
 
